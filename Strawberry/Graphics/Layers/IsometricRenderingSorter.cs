60cf567bbedde1cc33aaaee2e7259e66095f065b
koosha72 <koosha.abedini@gmail.com>
2025-03-23 06:49:59 +0330

    Changed lambda expression to a custom comparer for performance reasons

--- Strawberry/Graphics/Layers/IsometricRenderingSorter.cs
+++ Strawberry/Graphics/Layers/IsometricRenderingSorter.cs
@@ -1,10 +1,18 @@
 ﻿namespace Strawberry.Graphics.Layers
 {
+    public class IsometricComparer : IComparer<SpriteQuad>
+    {
+        public int Compare(SpriteQuad a, SpriteQuad b)
+        {
+            return (int)(a.XYUV1.Y - b.XYUV1.Y);
+        }
+    }
+
     public class IsometricRenderingSorter : IRenderingSorter
     {
         public void Sort(List<SpriteQuad> quads)
         {
-            quads.Sort((a, b) => (int)(a.XYUV1.Y - b.XYUV1.Y));
+            quads.Sort(new IsometricComparer());
         }
     }
 }
