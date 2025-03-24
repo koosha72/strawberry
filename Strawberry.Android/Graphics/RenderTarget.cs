c3ce1ce792a0b2f5ad160ec49916acaeec94b3a2
koosha72 <koosha.abedini@gmail.com>
2025-03-24 15:12:30 +0330

    - Android RenderTarget implemented

---
+++ Strawberry.Android/Graphics/RenderTarget.cs
@@ -0,0 +1,76 @@
+using Android.Opengl;
+using Strawberry.Graphics;
+
+namespace Strawberry.Android.Graphics;
+
+internal class RenderTarget : Base, IRenderTarget
+{
+    Texture texture;
+
+    int depthBuffer = 0;
+    int frameBuffer = 0;
+
+    public ITexture Texture
+    {
+        get { return texture; }
+        private set { texture = (Texture)value; }
+    }
+
+    public int Width
+    {
+        get { return texture.Width; }
+    }
+
+    public int Height
+    {
+        get { return texture.Height; }
+    }
+
+    internal int GLFrameBuffer { get { return frameBuffer; } }
+
+    GraphicsContext graphicsContext;
+    public IGraphicsContext GraphicsContext { get { return graphicsContext; } }
+
+    public RenderTarget(GraphicsContext gc, int width, int height)
+    {
+        texture = (Texture)gc.CreateTexture(width, height, (byte[])null);
+
+        int[] dbs = new int[1];
+        GLES30.GlGenRenderbuffers(1, dbs, 0);
+        depthBuffer = dbs[0];
+        GLES30.GlBindRenderbuffer(GLES30.GlRenderbuffer, depthBuffer);
+        GLES30.GlRenderbufferStorage(GLES30.GlRenderbuffer, GLES30.GlDepthComponent16, width, height);
+        GLES30.GlBindRenderbuffer(GLES30.GlRenderbuffer, 0);
+
+        int[] fbs = new int[1];
+        GLES30.GlGenFramebuffers(1, fbs, 0);
+        frameBuffer = fbs[0];
+        GLES30.GlBindFramebuffer(GLES30.GlFramebuffer, frameBuffer);
+        GLES30.GlFramebufferTexture2D(GLES30.GlFramebuffer, GLES30.GlColorAttachment0,
+            GLES30.GlTexture2d, texture.GLTexture, 0);
+        GLES30.GlFramebufferRenderbuffer(GLES30.GlFramebuffer, GLES30.GlDepthAttachment, GLES30.GlRenderbuffer, depthBuffer);
+
+        var status = GLES30.GlCheckFramebufferStatus(GLES30.GlFramebuffer);
+        if (status != GLES30.GlFramebufferComplete)
+        {
+            throw new Exception("Framebuffer generation failed with status code: " + status);
+        }
+
+        GLES30.GlBindFramebuffer(GLES30.GlFramebuffer, 0);
+    }
+
+    public void SetFilter(TextureFiltering minFilter, TextureFiltering magFilter)
+    {
+        Texture.SetFilter(minFilter, magFilter);
+    }
+
+    protected override void CleanManaged()
+    {
+        GLES30.GlDeleteRenderbuffers(1, new int[] { depthBuffer }, 0);
+        depthBuffer = 0;
+        Texture.Dispose();
+        GLES30.GlDeleteFramebuffers(1, new int[] { frameBuffer }, 0);
+        frameBuffer = 0;
+        base.CleanManaged();
+    }
+}
