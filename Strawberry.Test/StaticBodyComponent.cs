0c6e7ae78d3c87dfd7b395705a8bef532375b450
koosha72 <koosha.abedini@gmail.com>
2025-03-25 17:03:13 +0330

    - Added input test

--- Strawberry.Test/StaticBodyComponent.cs
+++ Strawberry.Test/StaticBodyComponent.cs
@@ -18,7 +18,7 @@ namespace Strawberry.Test
         {
             body = Scene.PhysicsWorld.CreateBody(Transform.Position / MyGameContext.ppm, 0, BodyType.Static);
             var size = (Sprite.Sprite.Size * Transform.Scale) / MyGameContext.ppm;
-            var f = body.CreateRectangle(size.X, size.Y, 1f, new Math.Vector2(2f,0.5f));
+            var f = body.CreateRectangle(size.X, size.Y, 1f, new Math.Vector2(2f, 0.5f));
             f.Restitution = 0.3f;
             f.Friction = 0.5f;
         }
@@ -26,6 +26,15 @@ namespace Strawberry.Test
         public override void OnFixedUpdate()
         {
             body.Position = Transform.Position / MyGameContext.ppm;
+        }
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+            if (GameContext.InputManager.PointingDevice.IsButtonDown(0, Input.PointerButtons.Primary))
+                Transform.Position += new Math.Vector2(0, 4 * MyGameContext.ppm) * FrameInfo.Information.DeltaTime;
+            if (GameContext.InputManager.PointingDevice.IsButtonDown(0, Input.PointerButtons.Secondary))
+                Transform.Position -= new Math.Vector2(0, 4 * MyGameContext.ppm) * FrameInfo.Information.DeltaTime;
         }
     }
 }
