d3ac25c2f48a7ac225aec119c45a99084c25deab
koosha72 <koosha.abedini@gmail.com>
2025-03-25 04:16:01 +0330

    - Made game fullscreen

--- Strawberry.Android.Test/MainActivity.cs
+++ Strawberry.Android.Test/MainActivity.cs
@@ -1,4 +1,6 @@
 using Strawberry.Test;
+using Android.Content.PM;
+using Android.Views;
 
 namespace Strawberry.Android.Test;
 
@@ -8,9 +10,17 @@ public class MainActivity : GameLauncher
     protected override void OnCreate(Bundle? savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
-
+        RequestedOrientation = ScreenOrientation.Landscape;
+        RequestWindowFeature(WindowFeatures.NoTitle);
+        Window.SetFlags(WindowManagerFlags.Fullscreen,
+            WindowManagerFlags.Fullscreen);
         MyGameContext stdGameContext = new MyGameContext();
         Game game = new Game();
+
+        View decorView = Window.DecorView;
+        var uiOptions = View.SystemUiFlagHideNavigation | View.SystemUiFlagImmersiveSticky;
+        decorView.SystemUiFlags = uiOptions;
+
         game.Run(stdGameContext, this);
     }
 }
