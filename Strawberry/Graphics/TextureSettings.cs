cf79ff94a10f9de8455409eb7fc7dbe06ca669c0
koosha72 <koosha.abedini@gmail.com>
2025-04-29 02:02:50 +0330

    - ToString, Equals, GetHashCode added

--- Strawberry/Graphics/TextureSettings.cs
+++ Strawberry/Graphics/TextureSettings.cs
@@ -18,4 +18,17 @@ public struct TextureSettings
         WrapS = TextureWrap.ClampToEdge;
         WrapT = TextureWrap.ClampToEdge;
     }
+
+    public override string ToString() =>
+    $"Format: {Format}, MinFilter: {MinFilter}, MagFilter: {MagFilter}, WrapS: {WrapS}, WrapT: {WrapT}";
+
+    public override bool Equals(object obj) => obj is TextureSettings other && this == other;
+    public override int GetHashCode() => HashCode.Combine(Format, MinFilter, MagFilter, WrapS, WrapT);
+    public static bool operator ==(TextureSettings a, TextureSettings b) =>
+        a.Format == b.Format &&
+        a.MinFilter == b.MinFilter &&
+        a.MagFilter == b.MagFilter &&
+        a.WrapS == b.WrapS &&
+        a.WrapT == b.WrapT;
+    public static bool operator !=(TextureSettings a, TextureSettings b) => !(a == b);
 }
