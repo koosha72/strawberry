7b82c9a32673433522c8ad48280c3393ff6f8d7e
koosha72 <koosha.abedini@gmail.com>
2026-05-28 14:29:19 +0330

    - Removed all test projects from the git.

--- Strawberry.Web.Test/Native/tsf.c
+++
@@ -1,8 +0,0 @@
-#ifdef _WIN32
-#define TSFDEF __declspec(dllexport)
-#else
-#define TSFDEF extern
-#endif
-
-#define TSF_IMPLEMENTATION
-#include "tsf.h"
--- Strawberry/Components/TransformComponent.cs
+++ Strawberry/Components/TransformComponent.cs
@@ -4,16 +4,35 @@ using Strawberry.Serialization;
 
 namespace Strawberry.Components
 {
+    /// <summary>
+    /// Specifies the coordinate space used to determine the position of a transform.
+    /// </summary>
     public enum PositionType
     {
+        /// <summary>
+        /// The position is relative to the parent entity's transform.
+        /// </summary>
         RelativeToParent,
+        
+        /// <summary>
+        /// The position is in global (world) coordinates.
+        /// </summary>
         Global
     }
 
+    /// <summary>
+    /// Manages the 2D transformation of an entity, including position, rotation, scale, and origin.
+    /// Supports parent-child hierarchies, allowing transformations to be inherited or orbit around a parent.
+    /// </summary>
     public class TransformComponent : BaseComponent
     {
         Vector2 localPosition = new Vector2();
 
+        /// <summary>
+        /// Gets or sets the world position of the entity. 
+        /// When getting, this calculates the final world position based on the <see cref="PositionType"/> and parent hierarchy.
+        /// When setting, this updates the <see cref="LocalPosition"/>.
+        /// </summary>
         [DoNotSerialize]
         public Vector2 Position
         {
@@ -47,6 +66,10 @@ namespace Strawberry.Components
         }
 
 
+        /// <summary>
+        /// Gets or sets the local position of the entity, relative to its parent.
+        /// If the entity has no parent, this is equivalent to the global position.
+        /// </summary>
         public Vector2 LocalPosition
         {
             get
@@ -59,10 +82,17 @@ namespace Strawberry.Components
             }
         }
 
+        /// <summary>
+        /// Gets or sets the 2D scale of the entity.
+        /// </summary>
         public Vector2 Scale { get; set; }
 
         float angle = 0;
 
+        /// <summary>
+        /// Gets or sets the rotation angle of the entity.
+        /// If <see cref="HeadAsParent"/> is true and a parent exists, getting this property returns the parent's angle.
+        /// </summary>
         public float Angle
         {
             get
@@ -84,22 +114,43 @@ namespace Strawberry.Components
             }
         }
 
+        /// <summary>
+        /// Gets the rotation direction of the entity as a double, equivalent to <see cref="Angle"/>.
+        /// </summary>
         public double Direction
         {
             get { return Angle; }
         }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether this entity should inherit the rotation angle from its parent entity.
+        /// </summary>
         public bool HeadAsParent { get; set; }
 
+        /// <summary>
+        /// Gets or sets the coordinate space used for this transform's position.
+        /// Defaults to <see cref="PositionType.Global"/> upon construction.
+        /// </summary>
         public PositionType PositionType { get; set; } = PositionType.RelativeToParent;
 
+        /// <summary>
+        /// Gets or sets the origin point (pivot) for rotation and scaling.
+        /// </summary>
         public Vector2 Origin { get; set; }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether the local position should be rotated by the parent's angle 
+        /// before applying the parent's offset. This creates an orbiting effect around the parent.
+        /// </summary>
         public bool RotateAroundParent { get; set; }
 
         /*[SceneEditorBounds]
         public RotatedRectangle Bounds { get { return new RotatedRectangle(Position, new Vector2(16f, 16f), Angle); } }*/
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TransformComponent"/> class.
+        /// Sets the default scale to (1, 1) and the <see cref="PositionType"/> to <see cref="PositionType.Global"/>.
+        /// </summary>
         public TransformComponent()
         {
             this.Scale = new Vector2(1f, 1f);
