9f7a7df0a78569d83ed04392bff04480418022b0
koosha72 <koosha.abedini@gmail.com>
2026-07-04 01:11:11 +0330

    - Fixed a bug in which when there is no source available to play a sound the Play method would fail

--- Strawberry.OpenAL/SoundBuffer.cs
+++ Strawberry.OpenAL/SoundBuffer.cs
@@ -54,14 +54,16 @@ namespace Strawberry.OpenAL
         public override Strawberry.Sound.Voice Play(bool loop = false, int priority = 0)
         {
             var v = soundManager.Play(this, 1.0f, loop);
-            v.Volume = 1f;
+            if (v != null)
+                v.Volume = 1f;
             return v;
         }
 
         public override Strawberry.Sound.Voice Play(float frequencyRatio = 1.0f, bool loop = false, int priority = 0)
         {
             var v = soundManager.Play(this, frequencyRatio, loop);
-            v.Volume = 1f;
+            if (v != null)
+                v.Volume = 1f;
             return v;
         }
 
